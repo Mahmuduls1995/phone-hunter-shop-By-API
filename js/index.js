c4b6072c3c88896bf1fabab290e02fb0ba12cf1b
mahmudul1995 <mahmuduls451@gmail.com>
2022-03-01 12:38:25 +0600

    code clean

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ document.getElementById("error-message").style.display="none";
 const searchPhones=()=>{
     const searchField=document.getElementById("search-field");
     const searchText=searchField.value;
-    //console.log(searchText);
     searchField.value=" ";
     document.getElementById('error-message').style.display = 'none'
     if (searchText== " ") {
@@ -31,16 +30,12 @@ const displaySearchResult=phones=>{
         
     }
     const first20Phones = phones.slice(0,20);
-    //console.log(first10Phones);
-
+   
     first20Phones.forEach(phone=>{
-        //console.log(phone.brand);
+        
         const div = document.createElement('div');
         
         div.classList.add('col')
-
-        
-        
             div.innerHTML=`
             <div class="card h-100 p-4 bg-secondary shadow bg-body rounded ">
             <img height="350px" src="${phone.image}" class="card-img-top" alt="...">
@@ -52,7 +47,6 @@ const displaySearchResult=phones=>{
             </div>
             `
             searchResult.appendChild(div);
-            //console.log(meals.length);
        
     })
 
@@ -65,8 +59,6 @@ const displaySearchResult=phones=>{
 
 }
 
-
-
 const detailsPhones=id => {
     const url=`https://openapi.programming-hero.com/api/phone/${id}`
     fetch(url)
@@ -76,19 +68,7 @@ const detailsPhones=id => {
 
 const detailsResult=details=>{
 
-    // const sensorDetails=details.mainFeatures.sensors;
-    // const sensorDetail=(sensorDetails)=>{
-    //     sensorDetails.forEach(sensor=>{
-    //        console.log(sensor);
-    //     })
-    
-    // }
-
-//     const sensorDetail=details.mainFeatures.sensors;
-//    const sensor= sensorDetail.forEach(sensor=>(sensor))
-
     const searchResult= document.getElementById("detail-result");
-    //console.log(details.mainFeatures.sensors[0]);
    
     const div = document.createElement('div');
         
@@ -120,34 +100,5 @@ const detailsResult=details=>{
 
 }
 
-        //    {/* <li class="list-group-item text-primary">Others: ${sensorDetails.forEach(sensor=>{sensor})}</li>
-        //             <li class="list-group-item text-primary">Others:${sensorDetail(sensorDetails)}</li> */}
-// const sensorDetail=()=>{
-//     const sensorDetail=details.mainFeatures.sensors;
-//      sensorDetail.forEach(sensor=>{
-//        console.log(sensor);
-//      })
-// }
 
-
-
-
-
-
-
-
-
-
-
-
-// if (${details.releaseDate.length== 0}) {
-                
-//     <h4 class="card-title">${details.releaseDate.innerText='no relese date'}</h4>
-    
-// }
-// else{
-
-// }
-//<p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
-//<button onclick="searchPhones()" class="btn btn-outline-secondary" type="button"id="button-search">search</button>
 
